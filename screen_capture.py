0c40a6f1cfd5d0d441d41f6184b8efe10a9e717e
Dillon Connolly <connollydillon@gmail.com>
2017-10-15 17:18:30 -0400

    HP detection
    New: The program is now able to detect changes in left and right HP bars. It isn't perfect but it is able to detect when the hp goes down.

--- screen_capture.py
+++ screen_capture.py
@@ -1,30 +1,56 @@
 import time
 
 import cv2
-import mss
-import numpy
+from mss.windows import MSS as mss
+import numpy as np
 
+def capture_img():
+    with mss.mss() as sct:
+        # Part of the screen to capture
+        leftHPCapture = {'top': 62, 'left': 84, 'width': 201, 'height': 12}
+        rightHPCapture = {'top': 62, 'left': 360, 'width': 201, 'height': 12}
 
-with mss.mss() as sct:
-    # Part of the screen to capture
-    monitor = {'top': 40, 'left': 0, 'width': 800, 'height': 640}
+        prevLeft = np.array(sct.grab(leftHPCapture))
+        prevRight = np.array(sct.grab(rightHPCapture))
+        # convert to grayscale
+        prevLeft = np.dot(prevLeft[...,:3], [0.299,0.587,0.114])
+        prevRight = np.dot(prevRight[...,:3], [0.299,0.587,0.114])
+        # round up negatives to 0
+        prevLeft = prevLeft.clip(min=0)
+        prevRight = prevRight.clip(min=0)
+        while 'Screen capturing':
+            #last_time = time.time()
+            # Get raw pixels from the screen, save it to a Numpy array
+            currLeft = np.array(sct.grab(leftHPCapture))
+            currRight = np.array(sct.grab(rightHPCapture))
 
-    while 'Screen capturing':
-        last_time = time.time()
+            # # Display the picture
+            # cv2.imshow('OpenCV/Numpy normal', currRight)
+            #
+            # # Press "q" to quit
+            # if cv2.waitKey(25) & 0xFF == ord('q'):
+            #     cv2.destroyAllWindows()
+            #     break
+            # convert to grayscale
+            currLeft = np.dot(currLeft[...,:3], [0.299,0.587,0.114])
+            currRight = np.dot(currRight[...,:3], [0.299,0.587,0.114])
+            # get the difference in previous vs current
+            diffLeft = prevLeft - currLeft
+            diffRight = prevRight - currRight
+            # round negative values up to 0
+            diffLeft = diffLeft.clip(min=0)
+            diffRight = diffRight.clip(min=0)
+            # Check for hit on left
+            if((diffLeft > 120).sum() > 10):
+                print('Left was hit')
+            # Check for hit on right
+            if((diffRight > 120).sum() > 10):
+                print('Right was hit')
+            # Set previous frame data to current frame data
+            prevLeft = currLeft
+            prevRight = currRight
+            # Sleep to reduce fps
+            time.sleep(0.15)
 
-        # Get raw pixels from the screen, save it to a Numpy array
-        img = numpy.array(sct.grab(monitor))
-
-        # Display the picture
-        cv2.imshow('OpenCV/Numpy normal', img)
-
-        # Display the picture in grayscale
-        # cv2.imshow('OpenCV/Numpy grayscale',
-        #            cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY))
-
-        print('fps: {0}'.format(1 / (time.time()-last_time)))
-
-        # Press "q" to quit
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            break
+if __name__ == "__main__":
+    capture_img()
