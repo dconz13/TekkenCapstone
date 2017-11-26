093ffdc29b141dc22400bae8cabb1b8152287967
Dillon Connolly <connollydillon@gmail.com>
2017-11-26 12:51:25 -0500

    Training complete
    Adding all of my model back ups and data.

--- numpy_data_format.py
+++ numpy_data_format.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas
 
-data = np.loadtxt('model backup/episodesAndRewards_1.txt')
-data2 = np.loadtxt('model backup/episodesAndRewards_2.txt')
-data3 = np.loadtxt('model backup/episodesAndRewards_3.txt')
+data = np.loadtxt('model backup/trial 3/episodesAndRewards_1.txt')
+data2 = np.loadtxt('model backup/trial 3/episodesAndRewards_2.txt')
+data3 = np.loadtxt('model backup/trial 3/episodesAndRewards_3.txt')
+#data4 = np.loadtxt('model backup/episodesAndRewards_4.txt')
+#data5 = np.loadtxt('model backup/episodesAndRewards_5.txt')
 
 
 test = data2.item((0,1))
@@ -11,12 +13,14 @@ test = data2.item((0,1))
 frame1 = pandas.DataFrame(data)
 frame2 = pandas.DataFrame(data2)
 frame3 = pandas.DataFrame(data3)
+#frame4 = pandas.DataFrame(data4)
+#frame5 = pandas.DataFrame(data5)
 
-frames = [frame1, frame2, frame3]
+frames = [frame1, frame2, frame3]#, frame4, frame5]
 
 hours_36 = pandas.concat(frames)
-writer = pandas.ExcelWriter('36_hour_test.xlsx')
-hours_36.to_excel(writer, 'Sheet1')
+writer = pandas.ExcelWriter('model backup/trial 3/36_hour_test.xlsx')
+hours_36.to_excel(writer, 'trial 3')
 writer.save()
 
 #print(hours_36)
